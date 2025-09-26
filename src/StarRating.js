d29d456819a82c03ad99be61578da32681cddf44
Milad <milad.soleymani1380@gmail.com>
2025-09-26 13:57:32 +0330

    fixing protocol of links

--- src/StarRating.js
+++ src/StarRating.js
@@ -46,7 +46,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
     >
       {full ? (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          xmlns="https://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="#fcc419"
           stroke="#fcc419"
@@ -55,7 +55,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
         </svg>
       ) : (
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          xmlns="https://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           stroke="#fcc419"
